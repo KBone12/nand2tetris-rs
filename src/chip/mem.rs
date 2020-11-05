3af1b2d718215e02bea1b0cdc5ea93c7b57243ff
kbone <kbonehobby@gmail.com>
2020-11-05 21:46:06 +0900

    Add Bit

--- src/chip/mem.rs
+++ src/chip/mem.rs
@@ -1,3 +1,5 @@
+use crate::chip::basic::mux;
+
 pub struct Dff {
     input: bool,
     output: bool,
@@ -29,6 +31,36 @@ impl Dff {
     }
 }
 
+pub struct Bit {
+    load: bool,
+    dff: Dff,
+}
+
+impl Bit {
+    pub fn new() -> Self {
+        Self {
+            load: false,
+            dff: Dff::new(),
+        }
+    }
+
+    pub fn set_load(&mut self, load: bool) {
+        self.load = load;
+    }
+
+    pub fn set_input(&mut self, input: bool) {
+        self.dff.set_input(mux(self.get_output(), input, self.load));
+    }
+
+    pub fn get_output(&self) -> bool {
+        self.dff.get_output()
+    }
+
+    pub fn set_clock(&mut self, clock: bool) {
+        self.dff.set_clock(clock);
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,6 +87,31 @@ mod tests {
                 dff.set_input(input);
                 dff.set_clock(clock);
                 assert_eq!(dff.get_output(), output);
+            });
+    }
+
+    #[test]
+    fn bit_changes_the_output_when_load_flag_is_on() {
+        let inputs = [
+            (false, false),
+            (false, true),
+            (true, false),
+            (true, true),
+            (false, false),
+            (false, true),
+        ];
+        let expected = [false, false, false, true, true, false];
+        let mut bit = Bit::new();
+
+        inputs
+            .iter()
+            .zip(expected.iter())
+            .for_each(|(&(input, load), &output)| {
+                bit.set_load(load);
+                bit.set_input(input);
+                bit.set_clock(true);
+                bit.set_clock(false);
+                assert_eq!(bit.get_output(), output);
             });
     }
 }
